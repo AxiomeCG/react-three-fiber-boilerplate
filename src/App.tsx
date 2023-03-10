b4d6eabb6c8440b5b756df8235ddecf7538a0877
Adam N <18304870+0xAxiome@users.noreply.github.com>
2023-03-10 18:00:26 +0100

    feat: add a default shader support for the react three fiber env

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import "./App.scss";
 import { OrbitControls, Sphere } from "@react-three/drei";
+import vertexShader from "./shaders/example/vertex.glsl"
+import fragmentShader from "./shaders/example/fragment.glsl"
+import { useMemo, useRef } from "react";
+import { ShaderMaterial } from "three";
+
+const Scene = () => {
+  const sphereRef = useRef<any>(null!);
+  const uniforms = useMemo(() => ({
+    uTime: {
+      value: 0.0
+    },
+    // Add any other attributes here
+  }), [])
+
+  useFrame((state) => {
+    const {clock} = state;
+
+    sphereRef.current.material.uniforms.uTime.value = clock.elapsedTime;
+  });
+
+  return <>
+    <Sphere ref={sphereRef}>
+      <shaderMaterial
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+        uniforms={uniforms}
+      />
+    </Sphere>
+  </>
+}
+
 
 function App() {
   return (
     <>
       <Canvas>
-        <Sphere>
-          <meshStandardMaterial color="red" />
-        </Sphere>
-        <pointLight position={[0, 5, 0]} intensity={1} color="white" />
-        <OrbitControls />
+        <Scene/>
+        <pointLight position={[0, 5, 0]} intensity={1} color="white"/>
+        <OrbitControls/>
       </Canvas>
     </>
   );
